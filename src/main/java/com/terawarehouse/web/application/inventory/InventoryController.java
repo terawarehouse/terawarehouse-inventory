a5584b1f6b3cc7691d33b61375a43939441b34b9
Edward P. Legaspi <czetsuya@gmail.com>
2019-10-27 08:35:35 +0800

    Update to Spring 2.2.0.RELEASE.

---
+++ src/main/java/com/terawarehouse/web/application/inventory/InventoryController.java
@@ -0,0 +1,16 @@
+package com.terawarehouse.web.application.inventory;
+
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Edward P. Legaspi | czetsuya@gmail.com
+ */
+@RestController
+@RequestMapping(path = "/categories", produces = MediaType.APPLICATION_JSON_VALUE)
+@Validated
+public class InventoryController {
+
+}
